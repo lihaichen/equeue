443753309cb251a54ba4ead48912a82f693205f7
lihaichen <li8303@163.com>
2018-02-22 15:20:25 +0800

    加入no_os的wait sleep

--- equeue_no_os.c
+++ equeue_no_os.c
@@ -1,6 +1,7 @@
 #include "equeue_config.h"
 #if defined(EQUEUE_PLATFORM_NO_OS)
 #include <sys/time.h>
+#include <unistd.h>
 
 equeue_tick_t equeue_tick() {
   struct timeval tv;
@@ -18,6 +19,9 @@ void equeue_mutex_unlock(equeue_mutex_t *m) {}
 int equeue_sema_create(equeue_sem_t *sem) { return 0; }
 void equeue_sema_destroy(equeue_sem_t *sem) {}
 void equeue_sema_signal(equeue_sem_t *sem) {}
-bool equeue_sema_wait(equeue_sem_t *sem, int us) { return false; }
+bool equeue_sema_wait(equeue_sem_t *sem, int us) {
+  usleep(1000);
+  return false;
+}
 
 #endif
