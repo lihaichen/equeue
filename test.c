5e43d0284b946e52ee60fb0c4574721c9ba0905e
lihaichen <li8303@163.com>
2018-02-22 15:05:54 +0800

    加入事件测试

--- test.c
+++ test.c
@@ -13,13 +13,21 @@ static void test_call_in(void *obj, void *data) {
 static void test_call_every(void *obj, void *data) {
   PRINT("call every ==>%s %s\r\n", (char *)obj, (char *)data);
 }
+static void test_event(void *obj, void *data) {
+  PRINT("test_event ==>%s %s\r\n", (char *)obj, (char *)data);
+}
+static void test_dispatch_event(void *obj, void *data) {
+  equeue_dispatch_event(&test_equeue, "test_event", "equeue_add_listener");
+}
 
 int main() {
-  equeue_create(&test_equeue, 3);
+  equeue_create(&test_equeue, 5);
   equeue_call(&test_equeue, test_call, "hello", "equeue_call!");
   equeue_call_in(&test_equeue, 10000, test_call_in, "hello", "equeue_call_in!");
   equeue_call_every(&test_equeue, 3000, test_call_every, "hello",
                     " equeue_call_every!");
+  equeue_add_listener(&test_equeue, "test_event", test_event, "hello");
+  equeue_call_every(&test_equeue, 5000, test_dispatch_event, NULL, NULL);
   equeue_run(&test_equeue, 0);
   return 0;
 }
