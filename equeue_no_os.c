4e79ea252ac02e038dfeaad1ea48b61046762092
lihaichen <li8303@163.com>
2018-02-22 10:37:55 +0800

    加入no os实现

---
+++ equeue_no_os.c
@@ -0,0 +1,18 @@
+#include "equeue_config.h"
+#if defined(EQUEUE_PLATFORM_NO_OS)
+
+equeue_tick_t equeue_tick() { return 0; }
+
+int equeue_mutex_create(equeue_mutex_t *m) { return 0; }
+void equeue_mutex_destroy(equeue_mutex_t *m) {}
+
+void equeue_mutex_lock(equeue_mutex_t *m) { __disable_irq(); }
+
+void equeue_mutex_unlock(equeue_mutex_t *m) { __enable_irq(); }
+
+int equeue_sema_create(equeue_sem_t *sem) { return 0; }
+void equeue_sema_destroy(equeue_sem_t *sem) {}
+void equeue_sema_signal(equeue_sem_t *sem) {}
+bool equeue_sema_wait(equeue_sem_t *sem, int us) { return false; }
+
+#endif
